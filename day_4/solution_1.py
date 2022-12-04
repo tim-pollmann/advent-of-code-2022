cc0c360b9dd488dd7483477f32b46f977ea5dd74
Tim Pollmann <pollmann.tim@protonmail.com>
2022-12-04 09:49:26 +0100

    day 4

---
+++ day_4/solution_1.py
@@ -0,0 +1,12 @@
+n = 0
+
+with open('input', 'r') as f:
+    while line := f.readline():
+        (lower1, upper1), (lower2, upper2) = [[int(x) for x in range.split('-')] for range in line.replace('\n', '').split(',')]
+        range1 = set(range(lower1, upper1 + 1))
+        range2 = set(range(lower2, upper2 + 1))
+        
+        if range1 <= range2 or range1 >= range2:
+            n += 1
+
+print(f'One range fully contains the other in {n} assignment pairs.')
