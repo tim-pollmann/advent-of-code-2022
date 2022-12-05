5035a5ff6fc056e643fcf997108993f4f716231a
Tim Pollmann <pollmann.tim@protonmail.com>
2022-12-05 10:31:09 +0100

    day 5

--- day_5/solution_1.py
+++ day_5/solution_1.py
@@ -1,7 +1,23 @@
-n = 0
+from typing import List
+import re
+
+
+stacks: List[List[str]] = [
+    ['S', 'T', 'H', 'F', 'W', 'R'],
+    ['S', 'G', 'D', 'Q', 'W'],
+    ['B', 'T', 'W'],
+    ['D', 'R', 'W', 'T', 'N', 'Q', 'Z', 'J'],
+    ['F', 'B', 'H', 'G', 'L', 'V', 'T', 'Z'],
+    ['L', 'P', 'T', 'C', 'V', 'B', 'S', 'G'],
+    ['Z', 'B', 'R', 'T', 'W', 'G', 'P'],
+    ['N', 'G', 'M', 'T', 'C', 'J', 'R'],
+    ['L', 'G', 'B', 'W']
+]
 
 with open('input', 'r') as f:
     while line := f.readline():
-        line = line.replace('\n', '')
-
-print(f'One range fully contains the other in {n} assignment pairs.')
+        amount, source_stack, target_stack = [int(i) for i in re.findall(r'\d+', line)]
+        stacks[target_stack - 1].extend(stacks[source_stack - 1][:- amount - 1:- 1]) 
+        stacks[source_stack - 1] = stacks[source_stack - 1][:- amount]
+        
+print(f'The code is "{str().join([stack.pop() for stack in stacks])}".')
---
+++ day_5/solution_2.py
@@ -0,0 +1,23 @@
+from typing import List
+import re
+
+
+stacks: List[List[str]] = [
+    ['S', 'T', 'H', 'F', 'W', 'R'],
+    ['S', 'G', 'D', 'Q', 'W'],
+    ['B', 'T', 'W'],
+    ['D', 'R', 'W', 'T', 'N', 'Q', 'Z', 'J'],
+    ['F', 'B', 'H', 'G', 'L', 'V', 'T', 'Z'],
+    ['L', 'P', 'T', 'C', 'V', 'B', 'S', 'G'],
+    ['Z', 'B', 'R', 'T', 'W', 'G', 'P'],
+    ['N', 'G', 'M', 'T', 'C', 'J', 'R'],
+    ['L', 'G', 'B', 'W']
+]
+
+with open('input', 'r') as f:
+    while line := f.readline():
+        amount, source_stack, target_stack = [int(i) for i in re.findall(r'\d+', line)]
+        stacks[target_stack - 1].extend(stacks[source_stack - 1][- amount:]) 
+        stacks[source_stack - 1] = stacks[source_stack - 1][:- amount]
+        
+print(f'The code is "{str().join([stack.pop() for stack in stacks])}".')
